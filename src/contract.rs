363cdd6c0de0cfa0e3ff16ca54afd1fe87fe7597
LoveNuna <marybarnett120@gmail.com>
2022-08-04 04:14:57 +0530

    wip

--- src/contract.rs
+++ src/contract.rs
@@ -3,7 +3,7 @@ use cosmwasm_std::{
     attr, coin, entry_point, to_binary, Addr, BankMsg, Binary, Coin, CosmosMsg, Deps, DepsMut, Env,
     MessageInfo, Response, StdError, StdResult, Storage, Uint128,
 };
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Timestamp};
 use cw2::set_contract_version;
 
 use cw20::{Balance, Cw20CoinVerified, Cw20ExecuteMsg, Cw20ReceiveMsg, Denom};
@@ -30,7 +30,7 @@ const MIN_STAKE_AMOUNT: u128 = 1;
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
@@ -50,130 +50,52 @@ pub fn instantiate(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
         ExecuteMsg::Stake {amount,denom,staker,} => stake(deps, env, info, amount, denom, staker),
-        ExecuteMsg::BeginRaffleRound {end_height_blocks} => begin_raffle_round(deps, env, info, end_height_blocks),
+        ExecuteMsg::BeginRaffleRound {begin_time_stamp, end_time_stamp, minimum_stake, winner_distribution,} => begin_raffle_round(deps, env, info, begin_time_stamp, end_time_stamp, minimum_stake, winner_distribution)
 }
 
 pub fn Stake(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     amount: u128,
     denom: Denom,
     staker: Addr,
 ) -> Result<Response, ContractError> {
     let state = STATE.load(deps.storage)?;
-    if state.owner != info.sender {
-        return Err(ContractError::Unauthorized);
-    }
-    let mut state = state.clone();
-    let mut state = state.stake(amount, denom, staker);
-    STATE.save(deps.storage, &state)?;
     Ok(Response::new())
 }
 
-pub fn BeginRaffleRound(
-    deps: DepsMut,
-    _env: Env,
-    info: MessageInfo,
-    end_height_blocks: u64,
-) -> Result<Response, ContractError> {
+pub fn getCurrentCounter(deps: DepsMut) -> Result<i32, ContractError> {
+    let counter = COUNTER.load(deps.storage)?;
+    Ok(counter.counter)
+}
+
+pub fn begin_raffle_round(deps: DepsMut, env: Env, info: MessageInfo, id: i32, endTimeStamp: Timestamp, players: Vec<Addr>, minimumStake: i32, winnerDistribution: Vec<i32>) -> Result<Response, ContractError> {
+    // let state = STATE.load(deps.storage)?;
+    let mut state = STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+        state.id = getCurrentCounter();
+        state.beginTimeStamp = env.block.time;
+        state.endTimeStamp = endTimeStamp;
+        state.players = players;
+        state.minimumStake = minimumStake;
+        state.winnerDistribution = winnerDistribution;
+        Ok(state)
+    })?;
+    Ok(Response::new().add_attribute("method", "begin_raffle_round"))
+}
+
+pub fn delete_raffle_round(deps: DepsMut, env: Env, info: MessageInfo, id: i32) -> Result<Response, ContractError> {
     let state = STATE.load(deps.storage)?;
-    if state.owner != info.sender {
-        return Err(ContractError::Unauthorized);
-    }
-    let mut state = state.clone();
-    let mut state = state.begin_raffle_round(end_height_blocks);
-    STATE.save(deps.storage, &state)?;
-    Ok(Response::new())
+    let mut state = STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+        state.id = id;
+        Ok(state)
+    })?;
+    Ok(Response::new().add_attribute("method", "delete_raffle_round"))
 }
-
-pub fn
-
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
-    }
-}
-
-fn query_count(deps: Deps) -> StdResult<GetCountResponse> {
-    let state = STATE.load(deps.storage)?;
-    Ok(GetCountResponse { count: state.count })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
-
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
-
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
-    }
 }
--- src/state.rs
+++ src/state.rs
@@ -1,24 +1,28 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Timestamp};
 use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
-}
-
 pub struct Raffle
 {
     pub id : i32,
-    pub beginTimeStamp : i64,
-    pub endTimeStamp : i64,
+    pub beginTimeStamp : Timestamp,
+    pub endTimeStamp : Timestamp,
     pub players: Vec<Addr>,
     pub winner : Vec<Addr>,
     pub minimumStake : i32,
-
+    pub winnerDistribution: Vec<i32>,
+    pub active: bool,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct UniversalCounter
+{
+    pub counter: i32,
+}
+
+pub const STATE: Item<Raffle> = Item::new("raffle");
+pub const COUNTER: Item<UniversalCounter> = Item::new("counter");
